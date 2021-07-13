9f35ccd7b4eef7fbb880f08c6b84ccfb48feafae
Elias Cuadra <e.cuadra@posteo.de>
2021-07-13 11:10:40 +0200

    progress 13.07.

--- data/Amprion/processing_amprion_2019.R
+++ data/Amprion/processing_amprion_2019.R
@@ -1,5 +1,5 @@
 ##########################################################################################################
-#Preparation of the master and movement data of the transmission system operator Amprion from 2017 - 2019#
+#Preparation of the master and movement data of the transmission system operator Amprion from 2015 - 2019#
 ##########################################################################################################
 
 #packages#
@@ -11,6 +11,8 @@ pacman::p_load(rio, data.table, tidyverse, tidyr, purrr, magrittr, compare,
 ################################
 #Import data and format columns#
 ################################
+amprion_2015 <- read.csv("EEG_StammBew_2015_Amprion-EAtlas.csv")
+amprion_2016 <- read.csv("EEG_StammBew_2016_Amprion-EAtlas.csv")
 amprion_2017 <- read.csv("EEG_StammBew_2017_Amprion-EAtlas.csv")
 amprion_2018 <- read.csv("EEG_StammBew_2018_Amprion-EAtlas.csv")
 amprion_2019 <- read.csv("EEG-StammBew_2019_Amprion-EAtlas.csv")
@@ -22,6 +24,8 @@ names(amprion_2017)[1] <- "gem"
 ####################
 #Variable selection#
 ####################
+selection_2015 <- amprion_2015[,c(1:3,8,10,17,27)]
+selection_2016 <- amprion_2016[,c(1:3,8,10,17,27)]
 selection_2017 <- amprion_2017[,c(1:3,8,10,17,27)]
 selection_2018 <- amprion_2018[,c(1:3,8,10,17,27)]
 selection_2019 <- amprion_2019[,c(1:3,7,9,16,26)]
@@ -30,6 +34,8 @@ selection_2019 <- amprion_2019[,c(1:3,7,9,16,26)]
 ###############################
 #formatting commissioning date#
 ###############################
+selection_2015$inbetriebnahme <- as.Date(selection_2015$inbetriebnahme, "%d/%m/%Y")
+selection_2016$inbetriebnahme <- as.Date(selection_2016$inbetriebnahme, "%d/%m/%Y")
 selection_2017$inbetriebnahme <- as.Date(selection_2017$inbetriebnahme, "%d/%m/%Y")
 selection_2018$inbetriebnahme <- as.Date(selection_2018$inbetriebnahme, "%d/%m/%Y")
 selection_2019$inbetriebnahme <- as.Date(selection_2019$inbetriebnahme, "%d/%m/%Y")
@@ -38,6 +44,8 @@ selection_2019$inbetriebnahme <- as.Date(selection_2019$inbetriebnahme, "%d/%m/%
 ###############
 #find outliers#
 ###############
+outliers_2015 <- filter(selection_2015, leistung < 100 | leistung > 4500 | inbetriebnahme > "2015-02-15")
+outliers_2016 <- filter(selection_2016, leistung < 100 | leistung > 4500 | inbetriebnahme > "2016-02-15")
 outliers_2017 <- filter(selection_2017, leistung < 100 | leistung > 4500 | inbetriebnahme > "2017-02-15")
 outliers_2018 <- filter(selection_2018, leistung < 100 | leistung > 4500 | inbetriebnahme > "2018-02-15")
 outliers_2019 <- filter(selection_2019, leistung < 100 | leistung > 4500 | inbetriebnahme > "2019-02-15")
@@ -46,6 +54,8 @@ outliers_2019 <- filter(selection_2019, leistung < 100 | leistung > 4500 | inbet
 ############################
 #create df without outliers#
 ############################
+selection_2015_without_outliers <- setdiff(selection_2015, outliers_2015)
+selection_2016_without_outliers <- setdiff(selection_2016, outliers_2016)
 selection_2017_without_outliers <- setdiff(selection_2017, outliers_2017)
 selection_2018_without_outliers <- setdiff(selection_2018, outliers_2018)
 selection_2019_without_outliers <- setdiff(selection_2019, outliers_2019)
@@ -267,6 +277,12 @@ selection_2019_counties_communities <- selection_2019_counties %>%
 #######################################################################################
 #add electricity yiel in MWh and full load hours in the selection_without_outlier df's#
 #######################################################################################
+selection_2015_without_outliers <- selection_2015_without_outliers %>% 
+  mutate(flh = menge_kwh/leistung) %>%
+  mutate(menge_mwh = round(menge_kwh/1000))
+selection_2016_without_outliers <- selection_2016_without_outliers %>% 
+  mutate(flh = menge_kwh/leistung) %>%
+  mutate(menge_mwh = round(menge_kwh/1000))
 selection_2017_without_outliers <- selection_2017_without_outliers %>% 
   mutate(flh = menge_kwh/leistung) %>%
   mutate(menge_mwh = round(menge_kwh/1000))
@@ -295,7 +311,7 @@ sum(comissioning_2001_2005$menge_kwh/10^6)
 #######################################
 #create flow chart of data preparation#
 #######################################
-#grViz(diagram = "digraph flowchart {
+grViz(diagram = "digraph flowchart {
   node [fontname = arial, shape = oval, fixedsize = FALSE]
   tab1 [label = '@@1', fontsize=30]
   tab2 [label = '@@2', fontsize=30]
@@ -387,7 +403,7 @@ pelectricity_yield_2019_poly <- ggplot() +
   theme_light() +
   scale_x_date(limits = as.Date(c("1990-01-01","2030-12-31"))) +
   ylim(-1000, 20000) +
-  xlab("Comissioning Date") +
+  xlab("Commissioning Date") +
   ylab("Electricity yield per WT and year [MWh]") +
   theme( axis.text=element_text(size=11),
          axis.title=element_text(size=12),
@@ -509,7 +525,7 @@ prated_capacity_over_commission <- ggplot() +
   theme_light() +
   scale_x_date(limits = as.Date(c("1990-01-01","2030-12-31"))) +
   ylim(-1000, 6000) +
-  xlab("Comissioning Date") +
+  xlab("Commissioning Date") +
   ylab("Rated capacity [kW]") +
   theme( axis.text=element_text(size=11),
          axis.title=element_text(size=12),
@@ -551,8 +567,16 @@ ggsave("results_of_analysis/rated_capacity_over_commissioning.png",
 
 
 #############################################################################
-#linear models with full load hours  over the commissioning date 2017 - 2019#
+#linear models with full load hours  over the commissioning date 2015 - 2019#
 #############################################################################
+#2015
+lm_flh_2015 <- lm(selection_2015_without_outliers$flh ~ selection_2015_without_outliers$inbetriebnahme)
+summary(lm_flh_2015)
+plot(lm_flh_2015)
+#2016
+lm_flh_2016 <- lm(selection_2016_without_outliers$flh ~ selection_2016_without_outliers$inbetriebnahme)
+summary(lm_flh_2016)
+plot(lm_flh_2016)
 #2017
 lm_flh_2017 <- lm(selection_2017_without_outliers$flh ~ selection_2017_without_outliers$inbetriebnahme)
 summary(lm_flh_2017)
@@ -570,51 +594,53 @@ plot(lm_flh_2019)
 #plot full load hours over commissioning date with trend#
 #########################################################
 pflh <- ggplot() +
+  geom_point(data = selection_2015_without_outliers, aes(x=inbetriebnahme, y=flh), size = 0.4, colour = "#fd00e2") +
+  geom_point(data = selection_2016_without_outliers, aes(x=inbetriebnahme, y=flh), size = 0.4, colour = "#fde600") +
   geom_point(data = selection_2017_without_outliers, aes(x=inbetriebnahme, y=flh), size = 0.4, colour = "#03a1fc") +
   geom_point(data = selection_2018_without_outliers, aes(x=inbetriebnahme, y=flh), size = 0.4, colour = "#fc5a03") +
   geom_point(data = selection_2019_without_outliers, aes(x=inbetriebnahme, y=flh), size = 0.4, colour = "#94fc03") +
+  geom_smooth(data = selection_2015_without_outliers, aes(x=inbetriebnahme, y=flh, colour = "2015"), method=lm, se=TRUE, fullrange = TRUE, size = 0.5)  +
+  geom_smooth(data = selection_2016_without_outliers, aes(x=inbetriebnahme, y=flh, colour = "2016"), method=lm, se=TRUE, fullrange = TRUE, size = 0.5)  +
   geom_smooth(data = selection_2017_without_outliers, aes(x=inbetriebnahme, y=flh, colour = "2017"), method=lm, se=TRUE, fullrange = TRUE, size = 0.5)  +
   geom_smooth(data = selection_2018_without_outliers, aes(x=inbetriebnahme, y=flh, colour = "2018"), method=lm, se=TRUE, fullrange = TRUE, size = 0.5)  +
   geom_smooth(data = selection_2019_without_outliers, aes(x=inbetriebnahme, y=flh, colour = "2019"), method=lm, se=TRUE, fullrange = TRUE, size = 0.5)  +
   theme_light() +
   scale_x_date(limits = as.Date(c("1990-01-01","2030-12-31"))) +
   ylim(0, 4000) +
-  xlab("Comissioning Date") +
-  ylab("Electricity yield per WT and year [MWh]") +
+  xlab("Commissioning Date") +
+  ylab("Full load hours [h/a]") +
   theme( axis.text=element_text(size=11),
          axis.title=element_text(size=12),
          plot.title = element_text(size=14),
          legend.position = c(0.85, 0.9),
          legend.direction = "horizontal") +
-  geom_hline(yintercept = 7400, linetype = 'dashed', size = 0.25) +
+  geom_hline(yintercept = 2300, linetype = 'dashed', size = 0.25) +
   annotate(geom="text",x=as.Date("1997-01-01"),
-           y=6500,label="mean of ~ 7,400 MWh/a in 2021 with linear trends", size = 2.5) +
-  geom_hline(yintercept = 10000, linetype = 'dashed', size = 0.25) +
+           y=2200,label="mean of ~ 2,300 h/a in 2021 with linear trends", size = 2.5) +
+  geom_hline(yintercept = 2800, linetype = 'dashed', size = 0.25) +
   annotate(geom="text",x=as.Date("2000-01-01"),
-           y=11000,label="mean of ~ 10,000 MWh/a in 2030 with linear trends", size = 2.5) +
+           y=2900,label="mean of ~ 2,800 h/a in 2030 with linear trends", size = 2.5) +
   geom_hline(yintercept = 15000, linetype = 'dashed', size = 0.25) +
   annotate(geom="text",x=as.Date("2003-01-01"),
            y=14200, label="> 15,000 MWh/a in 2030 with polynomial models", size = 2.5) +
   annotate(geom="text",x=as.Date("2026-01-01"),
-           y=2000,label= "All adj. R² (3rd poly) ~ 72 %", size = 2.5) +
-  annotate(geom="text",x=as.Date("2026-01-01"),
-           y=3500,label= "All R² (linear) ~ 68 - 70 %", size = 2.5) +
+           y=800,label= "All R²  ~ 46 - 51 %", size = 2.5) +
   annotate(geom="text",x=as.Date("2026-01-01"),
            y=400,label="p-values all << 0.001", size = 2.5) +
-  scale_colour_manual(name = "Year", values=c("#03a1fc", "#fc5a03", "#94fc03")) 
+  scale_colour_manual(name = "Year", values=c("#fd00e2", "#fde600", "#03a1fc", "#fc5a03", "#94fc03")) 
 
 
 
 ############
 #print plot#
 ############
-pflh +  theme(legend.position = c(0.25,0.9))
+pflh +  theme(legend.position = c(0.35,0.93))
 
 
 ####################
 #save plot as image#
 ####################
-ggsave("results_of_analysis/electricity_yield_2017-2019_rlp_over_comissioning_date2.png",
+ggsave("results_of_analysis/flh.png",
        plot = last_plot(),
        dpi = 900,
        width = 7,
